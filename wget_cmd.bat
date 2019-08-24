3680f7d7289c39ef2056154f128ffc24c7ca2e59
Kiehyun Kevin Park <guitar79@naver.com>
2019-08-24 14:22:37 +0900

    update

---
+++ wget_cmd.bat
@@ -0,0 +1,77 @@
+!/usr/bin/sh
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370396/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370395/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370394/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370393/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370392/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370391/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370390/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370389/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370388/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370387/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370386/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370385/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370384/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370383/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370382/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501370381/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501369068/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501369067/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501369066/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501369065/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501369064/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501369063/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501369062/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501369061/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501368407/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501368406/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501368405/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501368404/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501368403/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+
+
+##wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3 "https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501368408/" --header "Authorization: Bearer E47C7CDC-EAF7-11E8-A3FC-AFB1502E27AA" -P ./
+
+
+https://ladsweb.modaps.eosdis.nasa.gov/search/order/5/MYD04_3K--61/2019-01-01..2019-03-31/DB/Tile:H25V5,H26V5
+https://ladsweb.modaps.eosdis.nasa.gov/search/order/5/MOD04_3K--61/2019-01-01..2019-03-31/DB/Tile:H25V5,H26V5
+
+https://ladsweb.modaps.eosdis.nasa.gov/search/order/5/MOD04_3K--61/2019-01-01..2019-03-31/DB/Tile:H27V5,H28V5,H29V5
+https://ladsweb.modaps.eosdis.nasa.gov/search/order/5/MOD04_3K--61/2019-01-01..2019-03-31/DB/Tile:H25V4,H26V4
+https://ladsweb.modaps.eosdis.nasa.gov/search/order/5/MOD04_3K--61/2019-01-01..2019-03-31/DB/Tile:H27V4,H28V4
+
